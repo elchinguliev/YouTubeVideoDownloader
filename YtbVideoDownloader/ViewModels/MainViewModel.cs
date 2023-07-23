7af94bbc0f118df18c0b5911fe958ed1557b56f0
elchinguliev <elchingulievv@gmail.com>
2023-07-24 01:23:20 +0400

    Add project files.

---
+++ YtbVideoDownloader/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using YtbVideoDownloader.Commands;
+
+namespace YtbVideoDownloader.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainWindow MainWindow { get; set; }
+        public RelayCommand DownloadCommand { get; set; }
+        public MainViewModel(MainWindow mainWindow)
+        {
+            MainWindow=mainWindow;
+            DownloadCommand=new RelayCommand(obj =>
+            {
+                MessageBox.Show("Hele ki hazir deyil!");
+               
+                for (int i = 0; i < 100; i++)
+                {
+                    mainWindow.progressBar.Value++;
+                }
+            });
+        }
+    }
+}
